13fd9f8647df2311093e597803a3d7f9a22e4779
brian d foy <bdfoy@cpan.org>
2007-01-10 04:47:40 +0000

    Import of BDFOY/Test-Env-1.07 from CPAN.
    gitpan-cpan-distribution: Test-Env
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         BDFOY/Test-Env-1.07.tar.gz
    gitpan-cpan-author:       BDFOY
    gitpan-cpan-maturity:     released

--- lib/Env.pm
+++ lib/Env.pm
@@ -1,4 +1,4 @@
-# $Id: Env.pm,v 1.6 2006/05/18 02:30:03 comdog Exp $
+# $Id: Env.pm,v 1.7 2007/01/10 04:46:23 comdog Exp $
 package Test::Env;
 use strict;
 
@@ -6,7 +6,7 @@ use base qw(Exporter);
 use vars qw(@EXPORT $VERSION);
 
 @EXPORT  = qw(env_ok);
-$VERSION = sprintf "%d.%02d", q$Revision: 1.6 $ =~ m/ (\d+) \. (\d+) /xg;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.7 $ =~ m/ (\d+) \. (\d+) /xg;
 
 use Test::Builder;
 
@@ -75,12 +75,14 @@ members of the project can shepherd this module appropriately.
 
 brian d foy, C<< <bdfoy@cpan.org> >>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2002-2006, brian d foy, All rights reserved
+Copyright (c) 2002-2007 brian d foy.  All rights reserved.
 
-You can use this module under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
+
 
 "Roscoe";
